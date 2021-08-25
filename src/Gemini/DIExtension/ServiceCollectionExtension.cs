aa804f2fcab53dab3b384cc48de73a97b7f2fe33
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2021-08-25 21:07:44 +0800

    增加服务节点支持

--- src/Gemini/DIExtension/ServiceCollectionExtension.cs
+++ src/Gemini/DIExtension/ServiceCollectionExtension.cs
@@ -7,11 +7,15 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtension
     {
+        private static bool _done;
+        private static readonly object _lock;
         internal readonly static ConcurrentDictionary<Type, string> _optionsTypeCache;
         private readonly static MethodInfo _configWithNameMethodInfo;
         private readonly static MethodInfo _configWithoutNameMethodInfo;
         static ServiceCollectionExtension()
         {
+            _done = false;
+            _lock = new object();
             _optionsTypeCache = new ConcurrentDictionary<Type, string>();
             _configWithNameMethodInfo = typeof(OptionsConfigurationServiceCollectionExtensions).GetMethod("Configure", new Type[] { typeof(IServiceCollection), typeof(string), typeof(IConfiguration) });
             _configWithoutNameMethodInfo = typeof(OptionsConfigurationServiceCollectionExtensions).GetMethod("Configure", new Type[] { typeof(IServiceCollection), typeof(IConfiguration) });
@@ -23,7 +27,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="services"></param>
         /// <param name="configuration">配置实体</param>
-        public static IServiceCollection AddGeminiBuilder<TBuilder>(this IServiceCollection services,Func<TBuilder, TBuilder> builderAction = null) where TBuilder: IGeminiBuilder,new()
+        public static IServiceCollection AddGeminiBuilder<TBuilder>(this IServiceCollection services, Func<TBuilder, TBuilder> builderAction = null) where TBuilder : IGeminiBuilder, new()
         {
             TBuilder builder = new TBuilder();
             builder.SetServiceCollection(services);
@@ -40,57 +44,64 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <returns></returns>
         public static IServiceCollection AddGeminiOptions(this IServiceCollection services)
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var asm in assemblies)
+            if (!_done)
             {
-                if (!asm.IsDynamic)
+                lock (_lock)
                 {
-                    try
+                    if (!_done)
                     {
-                        var types = asm.GetTypes();
-                        foreach (var type in types)
-                        {
+                        _done = true;
 
-                            var optionsAttr = type.GetCustomAttribute<GeminiOptionsAttribute>();
-                            if (optionsAttr!=default)
+                        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                        foreach (var asm in assemblies)
+                        {
+                            if (!asm.IsDynamic)
                             {
-                                var positions = optionsAttr.Positions;
-                                if (positions.Length == 1)
+                                try
                                 {
-                                    var key = positions[0];
-#if DEBUG
-                                    Console.WriteLine($"注册配置 [{key}] 节点类型为 {type.Name}!");
-#endif
-                                    _optionsTypeCache[type] = key;
-                                    var methodInfo = _configWithoutNameMethodInfo.MakeGenericMethod(type);
-                                    methodInfo.Invoke(null, new object[] { services, IConfigurationBuilderExtension.Configuration.GetSection(key) });
-                                }
-                                else
-                                {
-                                    for (int i = 0; i < positions.Length; i += 1)
+                                    var types = asm.GetTypes();
+                                    foreach (var type in types)
                                     {
 
-                                        var key = positions[i];
+                                        var optionsAttr = type.GetCustomAttribute<GeminiOptionsAttribute>();
+                                        if (optionsAttr != default)
+                                        {
+                                            var positions = optionsAttr.Positions;
+                                            if (positions.Length == 1)
+                                            {
+                                                var key = positions[0];
 #if DEBUG
-                                        Console.WriteLine($"注册配置 [{key}] 节点类型为 {type.Name}!");
+                                                Console.WriteLine($"注册配置 [{key}] 节点类型为 {type.Name}!");
 #endif
-                                        _optionsTypeCache[type] = key;
-                                        var methodInfo = _configWithNameMethodInfo.MakeGenericMethod(type);
-                                        methodInfo.Invoke(null, new object[] { services, key, IConfigurationBuilderExtension.Configuration.GetSection(key) });
+                                                _optionsTypeCache[type] = key;
+                                                var methodInfo = _configWithoutNameMethodInfo.MakeGenericMethod(type);
+                                                methodInfo.Invoke(null, new object[] { services, IConfigurationBuilderExtension.Configuration.GetSection(key) });
+                                            }
+                                            else
+                                            {
+                                                for (int i = 0; i < positions.Length; i += 1)
+                                                {
 
+                                                    var key = positions[i];
+#if DEBUG
+                                                    Console.WriteLine($"注册配置 [{key}] 节点类型为 {type.Name}!");
+#endif
+                                                    _optionsTypeCache[type] = key;
+                                                    var methodInfo = _configWithNameMethodInfo.MakeGenericMethod(type);
+                                                    methodInfo.Invoke(null, new object[] { services, key, IConfigurationBuilderExtension.Configuration.GetSection(key) });
 
+                                                }
+                                            }
+
+                                        }
                                     }
                                 }
-
-
-                                
+                                catch (Exception ex)
+                                {
+                                    continue;
+                                }
                             }
-                            
                         }
-                    }
-                    catch (Exception ex)
-                    {
-                        continue;
                     }
                 }
             }
